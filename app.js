eadee578aef07f07af761099380b0d8c5410d366
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-04-08 21:29:56 +0430

    new info

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 "use strict";
-const myName = "Ali Agha";
-console.log(myName);
+const studentList = [
+    {
+        Name: "Ali",
+        lastName: "Alizada",
+        age: 23,
+        score: 90,
+    },
+    {
+        Name: "Ali aqa",
+        lastName: "Muhamadi",
+        age: 28,
+        score: 99,
+    },
+    {
+        Name: "Muhammad",
+        lastName: "Qasimi",
+        age: 33,
+        score: 89,
+    },
+];
--- app.ts
+++ app.ts
@@ -1,2 +1,26 @@
-const myName:string="Ali Agha";
-console.log(myName);
+type student={
+    Name:string,
+    lastName:string,
+    age:number,
+    score: number,
+};
+const studentList:student[]=[
+    {
+    Name:"Ali",
+    lastName:"Alizada",
+    age:23,
+    score:90,
+    },
+    {
+    Name:"Ali aqa",
+    lastName:"Muhamadi",
+    age:28,
+    score:99,
+    },
+    {
+    Name:"Muhammad",
+    lastName:"Qasimi",
+    age:33,
+    score:89,
+    },
+]
