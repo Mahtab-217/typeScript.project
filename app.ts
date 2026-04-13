5314f55498ed701b2b33e13e5bd25ec945b38797
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-04-13 21:59:24 +0430

    new codes

--- app.ts
+++ app.ts
@@ -1,41 +1,47 @@
-type student={
-    Name:string,
-    lastName:string,
-    age:number,
-    score: number,
-};
-const Students:Array<student>=[
-    {
-    Name:"Ali",
-    lastName:"Alizada",
-    age:23,
-    score:90,
-    },
-    {
-    Name:"Ali aqa",
-    lastName:"Muhamadi",
-    age:28,
-    score:99,
-    },
-    {
-    Name:"Muhammad",
-    lastName:"Qasimi",
-    age:33,
-    score:89,
-    },
-    {
-    Name:"Muhammad Ali",
-    lastName:"Qayomi",
-    age:54,
-    score:69,
-    },
-    {
-    Name:"Wafa Ali",
-    lastName:"Rahimi",
-    age:23,
-    score:98,
-    },
-]
+// type student={
+//     Name:string,
+//     lastName:string,
+//     age:number,
+//     score: number,
+// };
+// const Students:Array<student>=[
+//     {
+//     Name:"Ali",
+//     lastName:"Alizada",
+//     age:23,
+//     score:90,
+//     },
+//     {
+//     Name:"Ali aqa",
+//     lastName:"Muhamadi",
+//     age:28,
+//     score:99,
+//     },
+//     {
+//     Name:"Muhammad",
+//     lastName:"Qasimi",
+//     age:33,
+//     score:89,
+//     },
+//     {
+//     Name:"Muhammad Ali",
+//     lastName:"Qayomi",
+//     age:54,
+//     score:69,
+//     },
+//     {
+//     Name:"Wafa Ali",
+//     lastName:"Rahimi",
+//     age:23,
+//     score:98,
+//     },
+// ]
 
 
-const ArrayList:[string,number,boolean,number]=["Ahmad",43,false,45,];
+// const ArrayList:[string,number,boolean,number]=["Ahmad",43,false,45,];
+
+
+
+
+const number1:[number,number,number,number,number]=[5,6,7,8,9]
+const numbers:number[]=[1,2,3,4]
